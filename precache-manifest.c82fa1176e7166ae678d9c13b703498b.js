bd19a551d76b17d76ce9f6ddd22e6a92364758ef
Oluwaseyi <sheyilaaw98@gmail.com>
2019-05-31 21:36:04 +0100

    Updates

---
+++ precache-manifest.c82fa1176e7166ae678d9c13b703498b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c275b49c6e10ad2c8c10",
+    "url": "/react-redux-app/static/js/runtime~main.c275b49c.js"
+  },
+  {
+    "revision": "2c37ee9f05516b9d7503",
+    "url": "/react-redux-app/static/js/main.2c37ee9f.chunk.js"
+  },
+  {
+    "revision": "07a4d32b28151b97118e",
+    "url": "/react-redux-app/static/js/1.07a4d32b.chunk.js"
+  },
+  {
+    "revision": "2c37ee9f05516b9d7503",
+    "url": "/react-redux-app/static/css/main.4367361d.chunk.css"
+  },
+  {
+    "revision": "120d1630c4610bfafeb26d9a93505ccf",
+    "url": "/react-redux-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c206b261f98ca21e0f5cd8a286e0f22.js"
+  "/react-redux-app/precache-manifest.c82fa1176e7166ae678d9c13b703498b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-redux-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
